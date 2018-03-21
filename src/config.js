9368a381e7db47743ba2341b49efe7d64b952f73
kienht58 <kien.ht@teko.vn>
2018-03-21 17:32:49 +0700

    auto deploy

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const API_URL = process.env.NODE_ENV === 'production' ? 'https://walkbike.herokuapp.com/api' : 'http://localhost:8000/api'
+export const API_URL = process.env.NODE_ENV === 'production' ? 'https://walkbikes.com/api' : 'http://localhost:8000/api'
 
 export const GOOGLE_MAP_API_KEY = 'AIzaSyBDWNf6gvLGEJvsdTUU2plNtzqzdiifEEg'
 export const GOOGLE_MAP_JAVASCRIPT_API_URL = 'https://maps.googleapis.com/maps/api/js'
